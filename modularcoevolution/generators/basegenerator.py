589b005b89c3f4f2220a4fa01897a570900c08b6
Sean Harris <snh0037@auburn.edu>
2025-04-08 13:26:54 -0500

    Automatically added copyright header to all source files.

--- modularcoevolution/generators/basegenerator.py
+++ modularcoevolution/generators/basegenerator.py
@@ -4,6 +4,20 @@ Todo:
         It's used nowhere else, but might have been needed for CEADS-LIN
 
 """
+#  Copyright 2025 BONSAI Lab at Auburn University
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from typing import Any, Callable, Generic, TypeVar, Union
 
 import abc
