c417a66620e816e41dfdcd5884c5dcf56c339a2e
Sean Harris <snh0037@auburn.edu>
2026-01-22 13:49:02 -0600

    Rewrote `parallelutils.create_pool` to go back to using multiprocessing.Pool (which has better failure behavior), with a wrapper around it to pretend it's an Executor.

--- modularcoevolution/utilities/parallelutils.py
+++ modularcoevolution/utilities/parallelutils.py
@@ -17,6 +17,7 @@ __copyright__ = 'Copyright 2025, BONSAI Lab at Auburn University'
 __license__ = 'Apache-2.0'
 
 import concurrent.futures
+import itertools
 import logging
 import multiprocessing
 import os
@@ -52,8 +53,51 @@ def create_pool(num_processes: int = -1) -> concurrent.futures.Executor:
 
     if use_multiprocessing:
         _logger.info(f'Creating pool with {num_processes} processes.')
-        # TODO: ProcessPoolExecutor handles failure much worse than multiprocessing.Pool.
-        return concurrent.futures.ProcessPoolExecutor(max_workers=num_processes)
+        # ProcessPoolExecutor handles failure much worse than multiprocessing.Pool.
+        # return concurrent.futures.ProcessPoolExecutor(max_workers=num_processes)
+        return MultiprocessingPoolWrapper(processes=num_processes)
     else:
         _logger.info(f'Creating pool with {num_processes} threads (GIL is disabled).')
         return concurrent.futures.ThreadPoolExecutor(max_workers=num_processes)
+
+
+class MultiprocessingPoolWrapper:
+    """A simple wrapper around a multiprocessing.Pool to provide a similar interface to
+    concurrent.futures.Executor.
+    """
+    pool: multiprocessing.Pool
+
+    def __init__(self, processes: int):
+        self.pool = multiprocessing.Pool(processes=processes)
+
+    def map(self, fn, *iterables, chunksize: int = 1):
+        # concurrent.futures.Executor groups by argument position, then task index.
+        # Meanwhile, multiprocessing.Pool.starmap groups by task index, then argument position.
+        starmap_iterables = zip(*iterables)
+        wrapper_iterables = ((fn, args) for args in starmap_iterables)
+        return self.pool.imap(_starmap_wrapper, wrapper_iterables, chunksize=chunksize)
+
+    def shutdown(self, wait: bool = True, *, cancel_futures: bool = False):
+        if cancel_futures:
+            self.pool.terminate()
+        else:
+            self.pool.close()
+        if wait:
+            self.pool.join()
+
+    def __enter__(self):
+        return self.pool
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.pool.close()
+        else:
+            self.pool.terminate()
+        self.pool.join()
+        return False
+
+
+def _starmap_wrapper(fn_and_args):
+    """For use with `multiprocessing.Pool.imap` to make it function like `starmap`."""
+    function, args = fn_and_args
+    return function(*args)
