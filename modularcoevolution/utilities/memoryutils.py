e0c7c841c4d0ad7a95ed44b8708be19001a63cc0
Sean Harris <snh0037@auburn.edu>
2026-01-30 12:37:38 -0600

    Fixes a crash when `recursive_size` in `memoryutils` encounters an unbound function.

--- modularcoevolution/utilities/memoryutils.py
+++ modularcoevolution/utilities/memoryutils.py
@@ -1,4 +1,16 @@
 #  Copyright 2026 BONSAI Lab at Auburn University
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -58,7 +70,7 @@ def recursive_size(object: Any, seen_ids: set = None) -> Tuple[int, dict | None]
     elif _has_custom_sizeof(object):
         # Assume that a custom __sizeof__ is accurate (e.g. Numpy arrays)
         pass
-    elif hasattr(object, '__slots__'):
+    elif hasattr(object, '__slots__') and object.__slots__ is not None:
         items = {slot: getattr(object, slot) for slot in object.__slots__ if hasattr(object, slot)}
     elif hasattr(object, '__dict__'):
         items = object.__dict__
@@ -82,9 +94,11 @@ def _has_custom_sizeof(test_object: Any) -> bool:
     Returns:
         False if the object uses the default `__sizeof__` implementation from `object`, True otherwise.
     """
-    if not hasattr(test_object, 'mro'):
+    if not hasattr(type(test_object), 'mro'):
         return False
-    for cls in test_object.mro():
+    if isinstance(test_object, type):
+        return False
+    for cls in type(test_object).mro():
         if cls is object:
             pass
         elif '__sizeof__' in cls.__dict__:
